bf68adcc420dfcf6fa849281594a38dd440db2b7
Adrien Coulier <adrien.coulier@it.uu.se>
2018-03-21 08:44:58 +0100

    Add io features
    It is now possible to initialize a model from an input file, which has
    the same format as the output file, i.e. PID SID X Y Z.

--- src/RunGfrd/SimCustom.hpp
+++ src/RunGfrd/SimCustom.hpp
@@ -25,7 +25,10 @@ public:
       if (args.isparam(i) && args.option(i) == "m" && args.isvalue_NP(i + 1)) maintenance_step_ = std::stoi(args.option(++i));
       else if (args.isparam(i) && args.option(i) == "mf" && args.isvalue_F(i + 1)) simstate_file_ = args.option(++i);
       else if (args.isparam(i) && args.option(i) == "e" && args.isvalue_D(i + 1)) end_time_ = std::stod(args.option(++i));
+      else if (args.isparam(i) && args.option(i) == "seed" && args.isvalue_D(i + 1)) seed_ = std::stod(args.option(++i));
+      else if (args.isparam(i) && args.option(i) == "wsize" && args.isvalue_D(i + 1)) world_size_ = std::stod(args.option(++i));
       else if (args.isparam(i) && args.option(i) == "in" && args.isvalue_F(i + 1)) siminput_file_ = args.option(++i);
+      else if (args.isparam(i) && args.option(i) == "out" && args.isvalue_F(i + 1)) simoutput_file_ = args.option(++i);
       else return Simulation::HandleCommandArguments(i, args);
       return -1;
    }
@@ -39,6 +42,7 @@ public:
       std::cout << "        [-m N]                Maintenance every N steps\n";
       std::cout << "        [-mf file]            Maintenance output file\n";
       std::cout << "        [-in file]            Input file\n";
+      std::cout << "        [-out file]           Output file\n";
       std::cout << "        [-e time]             End simulation after model time in seconds\n";
    }
 
@@ -54,7 +58,7 @@ protected:
       std::cout << std::setw(14) << "time local = " << std::asctime(time_local);
 
       std::cout << "\n";
-      
+
       Simulation::PrintSettings();
 
       std::cout << "\n";
@@ -69,18 +73,6 @@ protected:
       DNA_bound = model_.add_species_type(SpeciesType("DNA_bound", model_.get_def_structure_type_id(), 0., 1e-9));
       Delta = model_.add_species_type(SpeciesType("Delta", model_.get_def_structure_type_id(), 1e-12, 1e-9));
       Notch = model_.add_species_type(SpeciesType("Notch", model_.get_def_structure_type_id(), 1e-12, 1e-9));
-
-      // Add some rules
-      // transcription
-      rules_.add_reaction_rule(ReactionRule(DNA, 100, std::vector < SpeciesTypeID > {DNA, Delta}));
-
-      // Degradation
-      rules_.add_reaction_rule(ReactionRule(Delta, 1, std::vector < SpeciesTypeID > {}));
-      rules_.add_reaction_rule(ReactionRule(Notch, 1, std::vector < SpeciesTypeID > {}));
-
-      //Binding/unbinding
-      rules_.add_reaction_rule(ReactionRule(ReactionRule::reactants(DNA, Notch), 10, std::vector < SpeciesTypeID > {DNA_bound}));
-      rules_.add_reaction_rule(ReactionRule(DNA_bound, 1, std::vector < SpeciesTypeID > {DNA, Notch}));
 
       // Create the world and simulator
       Simulation::SetupSimulation();
@@ -97,14 +89,37 @@ protected:
          world_.add_particle(SpeciesTypeID(sid), wid, Vector3(x, y, z));
       }
 
+      // Add some rules
+      // transcription
+      rules_.add_reaction_rule(ReactionRule(DNA, 100, std::vector < SpeciesTypeID > {DNA, Delta}));
+
+      // Degradation
+      rules_.add_reaction_rule(ReactionRule(Delta, 1, std::vector < SpeciesTypeID > {}));
+      rules_.add_reaction_rule(ReactionRule(Notch, 1, std::vector < SpeciesTypeID > {}));
+
+      //Binding/unbinding
+      rules_.add_reaction_rule(ReactionRule(ReactionRule::reactants(DNA, Notch), 10, std::vector < SpeciesTypeID > {DNA_bound}));
+      rules_.add_reaction_rule(ReactionRule(DNA_bound, 1, std::vector < SpeciesTypeID > {DNA, Notch}));
+
+      //Set up output file
+      pp_ = std::make_unique<ParticlePositions>(simulator_, end_time_);
+
+      simulator_->add_extrnal_event(0, pp_.get());
+      outstream.open(simoutput_file_, std::fstream::out | std::fstream::trunc);
+      pp_->set_output(outstream);
+
       return true;
    }
 
    // --------------------------------------------------------------------------------------------------------------------------------
 
    std::string siminput_file_;
+   std::string simoutput_file_;
+   std::fstream outstream;
+   std::unique_ptr<ParticlePositions> pp_;
 private:
    SpeciesTypeID DNA, DNA_bound, Delta, Notch;
+
 };
 
 // --------------------------------------------------------------------------------------------------------------------------------
